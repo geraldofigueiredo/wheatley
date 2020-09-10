74a446451ace8a10a6d5daa3a07c27da7aee4bff
gerfg <geraldo.figueiredosj@gmail.com>
2020-09-10 18:11:21 -0300

    feat: point implementation

---
+++ src/image/point.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Point {
+public:
+    typedef int Component;
+
+    Component x;
+    Component y;
+
+    Point(Component x, Component y): x{x}, y{y} {}
+    Point(): Point{0,0} {}
+    Point(const Point &other): Point{other.x, other.y} {}
+
+    void operator=(const Point &other) {
+        x = other.x;
+        y = other.y;
+    }
+};
